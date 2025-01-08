dbefcde9c893b341c0b810a61016292ebf637075
fedorivanko <fedorivanko7689@gmail.com>
2025-01-08 20:15:39 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import { Ticket } from "./Ticket";
 
 function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [isOpen, setIsOpen] = useState(true);
-
-  return <>{isOpen ? <Ticket /> : null}</>;
+  return <><Ticket /></>;
 }
 
 export default App;
